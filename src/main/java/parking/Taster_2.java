d82d7adbdf38a8c67cdfbaa27f95cebc27c5c553
Ruby_lin <rubylin0405@gmail.com>
2022-12-26 18:30:31 +0800

    new practice again

---
+++ src/main/java/parking/Taster_2.java
@@ -0,0 +1,61 @@
+package parking;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Taster_2 {
+    public static void main(String[] args) {
+//        java8Before();
+//        java8After();
+
+
+    }
+
+    private static void java8After() {
+        Instant instant = Instant.now();
+        System.out.println(instant);
+
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd   HH:mm:ss");
+        System.out.println("目前:" + dateTimeFormatter.format(now));
+
+        now.plus(Duration.ofHours(3));
+        System.out.println("失敗:" + dateTimeFormatter.format(now) + "\t+3分鐘");
+
+        System.out.println("成功:" + dateTimeFormatter.format(now.plus(Duration.ofHours(3))) + "\t+3分鐘");
+
+        LocalDateTime oneday = LocalDateTime.of(2000,2,20,8,8,20);
+        System.out.println(oneday);
+    }
+
+    private static void java8Before() {
+        Date date = new Date();
+        System.out.println(date);
+        System.out.println(date.getTime());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-DD   HH:mm:ss");
+        System.out.println(sdf.format(date));
+        String oneday = "2001-09-21   20:20:20";
+
+        try {
+            Date otherDay = sdf.parse(oneday);
+            System.out.println(otherDay);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getTime());
+        calendar.set(Calendar.MONTH,9);
+        System.out.println("更改月份:" + calendar.getTime());
+        calendar.add(Calendar.DAY_OF_MONTH,3);
+        System.out.println("增加天數:" + calendar.getTime());
+    }
+
+}
